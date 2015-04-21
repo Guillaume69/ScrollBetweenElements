66ed3688fe4a1b2a996f9c09ce697ff4e5e0e5f6
Guillaume Marchand <guillaume.marchand69@gmail.com>
2015-04-21 16:35:00 +0600

    add translate feature

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ScrollBetweenElements
-a jquery animate overlay that makes you scroll between HTML elements smoothly and without scrollbar
+a jquery animate overlay that makes you scroll between HTML elements smoothly and without scrollbar, it allows you to translate elements into your div parents aswell.
 
 simply link the script(make sure it is linked after jquery) and then enjoy(or not)
 
@@ -7,14 +7,21 @@ example:
 ``` html
 <div id="div1"></div>
 <div id="div2"></div>
-<div id="div3"></div>
+<div id="div3">
+	<div id="div4"></div>
+</div>
 ```
 ``` javascript
 var scroller = new ScrollBetweenElements(500, true);
-scroller.addElement("#div1");
-scroller.addElement("#div2");
-scroller.addElement("#div3");
+scroller.addElement("#div1", 0);
+scroller.addElement("#div2", 0);
+scroller.addElement("#div3", 0);
+scroller.addElement("#div4", 1);
 ```
+in the addElement method, the first parameter is the elements you want to animate, the second parameters indicate which animation you want. 0 means scroll from an element to the next one, 1 means translate from the right to the left. of course the elements you want to translate must have a 100% translation to the right to be hidden, and each elements (that you could call scene), must be 100% height and 100% height.
+
 simple right ?
+
+here is an example: http://guillaume-m.com/scrolldemo/
 
 of course you must have at least 2 elements, and if you're using it, you have to link all your elements, otherwise you won't be able to scroll between all the elements.
--- ScrollBetweenElements.js
+++ ScrollBetweenElements.js
@@ -1,66 +1,98 @@
 function ScrollBetweenElements(duration, isScrollbarHidden)
-    {
-      if (isScrollbarHidden){
-        var scrollPosition = [
-        self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-        self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-        ];
-        var html = jQuery('html');
-        html.data('scroll-position', scrollPosition);
-        html.css('overflow', 'hidden');
+{
+  if (isScrollbarHidden){
+    var scrollPosition = [
+    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+    self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var html = jQuery('html');
+    html.data('scroll-position', scrollPosition);
+    html.css('overflow', 'hidden');
+  }
+  var duration = duration;
+  var isFinished = true;
+  var elements = [];
+  var index = 0;
+  var previousIndex = 0;
+  var scrollDownFunctions = [];
+  scrollDownFunctions.push(scrollNext);
+  scrollDownFunctions.push(translateToZero);
+
+  function translateToZero()
+  {
+    $(elements[index]["element"]).animate({'left': '0'}, {
+      start: function(){
+        isFinished = false;
+        previousIndex = index;
+      },
+      done: function(){
+        isFinished = true;
+      },
+        duration:duration
+    },'linear');
+  }
+
+  function translate()
+  {
+    $(elements[index]["element"]).animate({'left': '100%'}, {
+      start: function(){
+        isFinished = false;
+        previousIndex = index;
+        --index;
+      },
+      done: function(){
+        isFinished = true;
+      },
+        duration:duration
+    },'linear');
+  }
+
+  function scrollNext()
+  {
+    $('body').animate({
+        scrollTop: $(elements[index]["element"]).offset().top
+      }, {
+        duration: duration,
+        start: function(){
+          isFinished = false;
+          previousIndex = index;
+        },
+        done: function(){
+          isFinished = true;
+        }
       }
-      var duration = duration;
-      var isFinished = true;
-      var elements = [];
-      var index = 0;
-      var previousIndex = 0;
-      this.addElement = function(element)
-      {
-        elements.push(element);
-      }
-      $(window).on("mousewheel", function(e) {
-           var oEvent = e.originalEvent,
-               delta  = oEvent.deltaY || oEvent.wheelDelta;
-           if (isFinished == true)
-           {
-             if (delta > 0 && previousIndex != ((elements.length - 1))) {
-               if (index < (elements.length - 1))
-                 ++index;
-               else
-                 --index;
-               $('body').animate({
-                   scrollTop: $(elements[index]).offset().top
-                   }, {
-                     duration: duration,
-                     start: function(){
-                       isFinished = false;
-                       previousIndex = index;
-                     },
-                     done: function(){
-                       isFinished = true;
-                     }
-                   }
-               );
-             }
-             if (delta <= 0 && previousIndex != 0){
+    );
+  }
+  this.addElement = function(element, elementType)
+  {
+    elements.push({"element": element, "elementType": elementType});
+  }
+  $(window).on("mousewheel", function(e) {
+    var oEvent = e.originalEvent;
+    var delta  = oEvent.deltaY || oEvent.wheelDelta;
+        if (isFinished == true)
+        {
+          if (delta > 0 && previousIndex != ((elements.length - 1)))
+          {
+            if (index < (elements.length - 1))
+              ++index;
+            else
+              --index;
+            scrollDownFunctions[elements[index]["elementType"]]();
+          }
+          if (delta <= 0)
+          {
+            if (elements[index]["elementType"] == 1)
+              translate();
+            else if (elements[index]["elementType"] == 0 && previousIndex != 0)
+            {
               if (index > 0)
-                 --index;  
-               else
-                 ++index;
-               $('body').animate({
-                   scrollTop: $(elements[index]).offset().top
-                   }, {
-                     duration: duration,
-                     start: function(){
-                       isFinished = false;
-                       previousIndex = index;
-                     },
-                     done: function(){
-                       isFinished = true;
-                     }
-                  }
-               );
+                --index;  
+              else
+                ++index;
+              scrollNext();
             }
           }
-      });
-    }
+        }
+  });
+}
